a1af94aef2816e1f50c1c9c0289a903baa613a2f
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2022-07-19 10:50:08 -0400

    removed comparison with twig cached

--- benchmarks.php
+++ benchmarks.php
@@ -5,7 +5,7 @@ $benchmarks = [];
 // Nate
 ob_start();
 $t1 = microtime(true);
-require_once __DIR__.'/nate/index.php';
+require __DIR__.'/nate/index.php';
 $t2 = microtime(true);
 ob_end_clean();
 
@@ -14,24 +14,13 @@ $benchmarks['nate'] = $t2 - $t1;
 // Twig
 ob_start();
 $t1 = microtime(true);
-require_once __DIR__.'/twig/index.php';
+require __DIR__.'/twig/index.php';
 $t2 = microtime(true);
 ob_end_clean();
 
 $benchmarks['twig'] = $t2 - $t1;
 
-// Twig (using cache)
-$twigCache = true;
-
-ob_start();
-$t1 = microtime(true);
-require_once __DIR__.'/twig/index.php';
-$t2 = microtime(true);
-ob_end_clean();
-
-$benchmarks['twig_cache'] = $t2 - $t1;
-
-ksort($benchmarks);
+asort($benchmarks);
 
 var_dump($benchmarks);
 
