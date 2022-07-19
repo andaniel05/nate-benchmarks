61afddc2aa30bdb96d44338c1ec8384bd170093f
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2022-07-19 09:08:55 -0400

    created twig benchmark

--- benchmarks.php
+++ benchmarks.php
@@ -2,6 +2,7 @@
 
 $benchmarks = [];
 
+// Nate
 ob_start();
 $t1 = microtime(true);
 require_once __DIR__.'/nate/index.php';
@@ -9,6 +10,15 @@ $t2 = microtime(true);
 ob_end_clean();
 
 $benchmarks['nate'] = $t2 - $t1;
+
+// Twig
+ob_start();
+$t1 = microtime(true);
+require_once __DIR__.'/twig/index.php';
+$t2 = microtime(true);
+ob_end_clean();
+
+$benchmarks['twig'] = $t2 - $t1;
 
 ksort($benchmarks);
 
---
+++ twig/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
+$twig = new \Twig\Environment($loader);
+$page = $twig->load('page.twig');
+
+echo $page->render([
+    'lang' => 'en',
+    'title' => 'My Title',
+    'persons' => [
+        ['name' => 'Juan Pérez', 'age' => '50', 'gender' => 'M'],
+        ['name' => 'Luisa Martínez', 'age' => '23', 'gender' => 'F'],
+        ['name' => 'Manuel Pérez', 'age' => '30', 'gender' => 'M'],
+    ],
+    'footer_content' => '<p style="background-color: gray; color: white">This is the footer content.</p>',
+]);
